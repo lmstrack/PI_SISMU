45ac563aa716773825ddd28c175c8a3df6f8dee1
LUCASSTRACK <LUCASSTRACK@DESKTOP-UTB2E5D>
2019-05-05 15:43:10 -0300

    Cadastro de Exposicao

---
+++ src/java/br/feevale/projetosismu/entity/Exposicao.java
@@ -0,0 +1,53 @@
+
+package br.feevale.projetosismu.entity;
+
+import java.sql.Date;
+
+public class Exposicao {
+
+    private Integer idExposicao;
+    private Date dataInicio;
+    private Date dataFim;
+    private Integer codExpositor;
+    private String codUnidade;
+
+    public Integer getIdExposicao() {
+        return idExposicao;
+    }
+
+    public void setIdExposicao(Integer idExposicao) {
+        this.idExposicao = idExposicao;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Integer getCodExpositor() {
+        return codExpositor;
+    }
+
+    public void setCodExpositor(Integer codExpositor) {
+        this.codExpositor = codExpositor;
+    }
+
+    public String getCodUnidade() {
+        return codUnidade;
+    }
+
+    public void setCodUnidade(String codUnidade) {
+        this.codUnidade = codUnidade;
+    }
+}
